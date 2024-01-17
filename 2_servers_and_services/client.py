d331f1340046f49d05c47bacca5a82226011d7d3
laaledesiempre <alejandrabotacura@outlook.com.ar>
2024-01-17 10:08:07 -0300

    solved problems w client and service

--- 2_servers_and_services/client.py
+++ 2_servers_and_services/client.py
@@ -1,24 +1,24 @@
 import requests
 
-usuarios=requests.get("http://localhost:3001/usuarios")
+usuarios=requests.get("http://localhost:8001/usuarios")
 print(usuarios.text) # tambien podriamos verlo como json con .json
 
 manolin={
     "nombre":"Manolito",
     "apodo":"Manolin"
 }
-agregando_usuario=requests.post("http://localhost:3001/usuarios",json=manolin)
+agregando_usuario=requests.post("http://localhost:8001/usuarios",json=manolin)
 
 print("Usuarios luego de agregar a manolin")
-print(request.get("http://localhost:3001/usuarios").text)
+print(requests.get("http://localhost:8001/usuarios").text)
 
 manolin["apodo"]="Manolino"
-modificando_usuario=requests.put("http://localhost:3001/usuarios",json=manolin)
+modificando_usuario=requests.put("http://localhost:8001/usuarios",json=manolin)
 
 print("Usuarios luego de modificar a manolin")
-print(request.get("http://localhost:3001/usuarios").text)
+print(requests.get("http://localhost:8001/usuarios").text)
 
-borrando_usuario=requests.delete("http://localhost:3001/usuarios",json={"nombre":"Manolito"})
+borrando_usuario=requests.delete("http://localhost:8001/usuarios",json={"nombre":"Manolito"})
 
 print("Usuarios luego de borrar a manolin")
-print(request.get("http://localhost:3001/usuarios").text)
+print(requests.get("http://localhost:8001/usuarios").text)
--- 2_servers_and_services/server.py
+++ 2_servers_and_services/server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__) 
 
-usuarios= {} # nuestros usuarios!, la estructura va a ser "nombre":"apodo"
+usuarios= {} # nuestros usuarios!, la estructura va a ser ['nombre']":['apodo']"
 
 @app.get("/usuarios")
 def traer_usuarios():
@@ -11,20 +11,26 @@ def traer_usuarios():
 @app.post("/usuarios") # cambiamos get por post!
 def definir_usuarios():
     print(request.get_json()) #Para ver la llamada!
-    usuarios[request.nombre]=request.apodo
+    request_json=request.get_json()
+    usuarios[request_json['nombre']]=request_json["apodo"]
+    print(usuarios)
+    return "hii"
      
-
 @app.put("/usuarios") # cambiamos get por post!
-def definir_usuarios():
+def cambiar_usuarios():
     print(request.get_json()) #Para ver la llamada!
-    usuarios[request.nombre]=request.apodo
+    request_json=request.get_json()
+    usuarios[request_json['nombre']]=request_json["apodo"]
+    return "Usuario cambiado"
       
 
 @app.delete("/usuarios") # cambiamos get por post!
-def definir_usuarios():
+def borrar_usuarios():
     print(request.get_json()) #Para ver la llamada!
-    del usuarios[request.nombre]
+    request_json=request.get_json()
+    del usuarios[request_json['nombre']]
+    return "usuario borrado"
       
-
 if __name__ == '__main__':
     app.run(port=8001) 
+
